acecd363a09a885dde375ad58ff626ad1618eabf
max2007killer <50517869+max2007killer@users.noreply.github.com>
2022-12-25 22:23:08 +0300

    Update CHINDRACHINDRA.lua

---
+++ CHINDRACHINDRA.lua
@@ -0,0 +1,9 @@
+local target = game:GetService("Players").case57946
+local message = "извеняйся за слова"
+local gives_message = true
+if gives_message then
+    target:Kick(message)
+else
+    target:Kick()
+    
+    
